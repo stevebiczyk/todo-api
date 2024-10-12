8df6d7fc49ad7335d5a6b0cbe9ba4e72751f6084
stevebiczyk <stevebiczyk@gmail.com>
2024-10-12 09:51:04 +0100

    set up and initialised MySQL database

--- index.ts
+++ index.ts
@@ -1,18 +1,52 @@
-import express, { Express, Request, Response } from 'express';
+import express, {
+  Express,
+  Request,
+  Response,
+} from 'express';
 import dotenv from 'dotenv';
-// import { dot } from 'node:test/reporters';
+import { DataSource } from 'typeorm';
 
 // Initiate express app
 const app: Express = express();
 dotenv.config();
+
+// Log environment variables
+console.log('Environment Variables:');
+console.log('MYSQL_USER:', process.env.MYSQL_USER);
+console.log('MYSQL_PASSWORD:', process.env.MYSQL_PASSWORD);
+console.log('MYSQL_DB:', process.env.MYSQL_DB);
+
+// Create database connection
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
 
 // Define server port
 const port = process.env.PORT;
 
 // Create a default route
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express & TypeScript Cathats');
+  res.send('Express & TypeScript Server');
 });
 
-// Start listening to requests on the defined port
-app.listen(port);
+console.log('DataSource options:', AppDataSource.options);
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(port);
+    console.log(
+      'Data source is initialised, app is listening on port',
+    );
+  })
+  .catch((err) => {
+    console.error(
+      'Error during data source initialisation',
+      err,
+    );
+  });
