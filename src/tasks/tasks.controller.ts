62c0607694af44efbe5f2a5c19257dfd9250b1d2
stevebiczyk <stevebiczyk@gmail.com>
2024-10-13 12:50:31 +0100

    moved validation logic to Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,8 +2,10 @@ import { AppDataSource } from '../../index';
 import { Task } from './tasks.entity';
 import { instanceToPlain } from 'class-transformer';
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 
 class TasksController {
+  // Method for the get route
   public async getAll(
     req: Request,
     res: Response,
@@ -31,6 +33,36 @@ class TasksController {
         .status(500);
       //   throw new Error('Error fetching tasks');
     }
+  }
+
+  // Method for the post route
+
+  public async create(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
+    // Create a new task instance
+    // const newTask = new Task();
+    // newTask.title = req.body.title;
+    // newTask.description = req.body.description;
+    // newTask.date = new Date();
+    // //Add the required properties to the new task instance
+    // // Save the task to the database
+    // try {
+    //     await AppDataSource.getRepository(Task).save(newTask);
+    //     return res.json(newTask).status(201);
+    // } catch (errors) {
+    //     return res
+    //         .json({ error: 'Internal Server Error' })
+    //         .status(500);
+    // }
   }
 }
 
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,7 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { taskController } from './tasks.controller';
 import { createValidator } from './tasks.validator';
-import { validationResult } from 'express-validator';
 
 // Fire the router function
 export const tasksRouter: Router = Router();
@@ -12,16 +11,5 @@ tasksRouter.get('/tasks', taskController.getAll);
 tasksRouter.post(
   '/tasks',
   createValidator,
-  //@ts-ignore
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res
-        .status(400)
-        .json({ errors: errors.array() });
-    }
-    // const taskController = new TaskController();
-    // const newTask = await taskController.create(req.body);
-    // res.json(newTask).status(201);
-  },
+  taskController.create,
 );
